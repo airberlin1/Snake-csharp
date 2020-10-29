83accd6a2c7d5930a1d448c71ef49d88093f6859
airberlin1 <66678722+airberlin1@users.noreply.github.com>
2020-10-29 14:47:43 +0100

    Update Snake.cs

--- Snake.cs
+++ Snake.cs
@@ -1,12 +1,33 @@
+# include <SPI.h>
+# include <Wire.h>
+# include <Adafruit_GFX.h>
+# include <Adafruit_SSD1306.h>
+# include "SenseBoxMCU.h"
+    
+# define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+
 void setup() {
+    // init snake
     boolean continue = true;
     boolean button_pressed = false;
     Snake snake = new Snake();
+    // TODO what does this do
     pinMode(7, OUTPUT);
     digitalWrite(7,HIGH);
+    // init time
     int time = millis();
-
+    String[] DIRECTIONS = new String[] {"right", "left", "up", "down"};
+    // display setup
+    senseBoxIO.powerI2C(true);
+    delay(2000);
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
+    display.display();
+    delay(100);
+    display.clearDisplay();
 }
+
 void loop() {
     continue = useSensorInput(continue);
     if (millis() - time > 500 && continue){
@@ -27,10 +48,11 @@ public boolean useSensorInput() {
 public class Snake{
     public Snake() {
       int[] coord = new int[] {15, 15};
-      String direction = "right";
+      SnakeSegment[] coords = new SnakeSegment[];
+      String directionHeading = "right";
     }
     public void move(){
-      switch (direction) {
+      switch (directionHeading) {
           case "right":
               coord[0] += 1;
           case "left":
@@ -39,12 +61,18 @@ public class Snake{
               coord[1] += 1;
           case "down":
               coord[1] += 1;
+          default:
+              break;
       }
     }
     public void show(){
-
+        // fillRect(x, y, width, height, full(bool))
+        display.fillRect();
+        display.display();
     }
     public void setDirection(dir) {
       direction = dir;
     }
+}
+public class SnakeSegment {
 }
